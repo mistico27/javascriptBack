1d91c60881a5f627a22c6d41b96b129f0fa55a3b
chris_beltran <cristianb72@hotmail.com>
2023-05-31 20:01:33 -0600

    letras

--- app.js
+++ app.js
@@ -1,52 +1,41 @@
-const { default: mongoose } = require('mongoose');
-const mngoose = require('mongoose');
-const url='mongodb+srv://cristianb72:30vxY4BnKpy54mj7@cluster0.wxjolyf.mongodb.net/nuevoSchema?retryWrites=true&w=majority'
-mongoose.connect(url,{
+const rgv = require('yargs-parser')(process.argv.slice(2))
+const colors = require('colors');
+ 
 
-}).then(()=>{console.log("estoy conectado a la abse  de datos de mongo putos");})
-.catch((e)=>console.log("El error de conexion es " +e))
+console.log("rgv",rgv);
 
-const nuevoesquema =mngoose.Schema({
-    nombre:String,
-    edad:Number,
-    pais:String
-})
+const nombres = [
+    "Andres",
+    "Andres de Anda",
+    "Christian",
+    "Damian",
+    "Dani",
+    "Diego",
+    "Dan",
+    "Gustavo",
+    "Ignacio",
+    "Jorge",
+    "Karla",
+    "Lau",
+    "Luis",
+    "Mara",
+    "Max",
+    "Pao",
+    "Roberto",
+    "Samantha"
+  ]
 
-const personasModel =mngoose.model('nuevoschemas',nuevoesquema)
-///mostrarnuevoSchema
+  const nombre =rgv.nombre;
 
-const mostrar = async()=>{
-    const personas =await personasModel.find();
-    console.log("hey"+personas);
-}
-
-///crear
-const crear = async ()=>{
-    const persona = new personasModel({
-        nombre:'Pedro',
-        edad:34,
-        pais:"Argentina"
-    })
-    const resultado = await persona.save();
-    console.log(resultado);
-}
-
-//Editar
-const actualizar = async (id)=>{
-    const persona = await personasModel.updateOne({_id:id},{
-        $set:{
-            nombre:'Pedro Modificado',
-            edad:67,
+  function sayHelloOrVete (nombre){
+    
+        if(nombres.includes(nombre)){
+            console.log(colors.rainbow(`Bienvenido ${nombre}`));
+            
+        }else{
+            console.log(colors.red(`largate ${nombre}`  ));
         }
-    })
-}
-//const borrar
-const eliminar =async (id)=>{
-    const persona = await personasModel.deleteOne({_id:id})
-    console.log(persona);
-}
+    
+  }
 
-//actualizar('64771e6659ff9a9794114ced');
-//crear();
-eliminar('64771f92c9bfc7014ddba23e');
-mostrar();
+  sayHelloOrVete(nombre);
