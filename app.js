f40c4a6204c323626366367f25ee50b417df41b2
chris_beltran <cristianb72@hotmail.com>
2023-06-05 22:23:54 -0600

    madrid

--- app.js
+++ app.js
@@ -1,63 +1,29 @@
-/*const http = require("http");
-
-// Declarando el servidor
-
-
-const server = http.createServer((request, response) => {
-    // Leer la request
-    const { url, method } = request;
-    const endpoints = {
-        GET: {
-          "/hola": "Mensaje totalmente diferente de los demas",
-          "/adios": "Mensaje totalmente diferente de los demas con ADIOS",
-        },
-        POST: {
-          "/hola": "Rita de post"
-        },
-        PATCH: {
-          "/hola": "Ruta de patch en holaaaa"
-        },
-        DELETE: {
-          "/adios": "Ruta adioooos delete"
-        }
-    };
-
-
-    response.write(endpoints[method][url]);
-    // Responder
-    response.end();
-});
-
-// Prender nuestro servidor
-server.listen(8080, () => {
-    console.log("Nuestro servidor esta prendido!!!");
-});
-*/
-
 const express = require('express')
-let app = express();
+let app= express();
+const fsPromises = require("fs/promises");
 
 
-app.get("/",function(req,res){
-    res.write("hola como andan amiguitos estoy en home")
-    res.end();
+app.get("/",(req,res)=>{
+	res.send("mensaje desde el nuevo rest")
 })
 
-app.patch("/home",function(req,res){
-    res.write("Hey amigos estoy a punto de hacer un update")
-    res.end();
-})
 
-app.post("/home",function(req,res){
-    res.write("estoy enviando esta infor con post")
-    res.end();
-})
+app.get("/getAllKoders", async (request, response) => {
+    const data = await fsPromises.readFile("./users.json", "utf-8")
+    response.send(data);
+  });
 
-app.delete("/home",function(req,res){
-    res.write("te eliminare con mi Api")
-    res.end();
-})
-
-app.listen(8080, () => {
-    console.log("Nuestro servidor esta prendido, is on fire!!!");
+app.get("/koders/:name", async (req, res) => {
+    // Path param
+    const { name } = req.params;
+    const db = await fsPromises.readFile("./users.json", "utf8");
+    const parsedDb = JSON.parse(db);
+    const filteredKoder = parsedDb.koders.filter(
+        (koder) => koder.name.toLowerCase() === name.toLowerCase()
+    )[0];
+    res.json(filteredKoder);
 });
+
+app.listen(8000,()=>{
+    console.log("Estoy conectado")
+})
