b92aaf9cdcddbf777a7a63be2734c5fa26f297d1
chris_beltran <cristianb72@hotmail.com>
2023-06-06 20:34:04 -0600

    ejercicios

--- app.js
+++ app.js
@@ -8,10 +8,57 @@ app.get("/",(req,res)=>{
 })
 
 
-app.get("/getAllKoders", async (request, response) => {
-    const data = await fsPromises.readFile("./users.json", "utf-8")
-    response.send(data);
-  });
+  app.get("/getAllKoders", async (req, res) => {
+    const { module } = req.query;
+    console.log("req.query", req.query)
+    const db = await fsPromises.readFile("./users.json", "utf8"); // leemos base de datos
+    const parsedDB = JSON.parse(db); // parseamos json
+    const filteredKoders = parsedDB.koders.filter(koder => module === koder.module)
+    // Esta vacio, no filtro
+    if(filteredKoders.length === 0) {
+        res.json(parsedDB.koders)
+    } else {
+        res.json(filteredKoders); // respondemos con header de Content-Type -> application/json
+    }
+});
+
+/**
+ * Ejercicio
+ * 2 endpoints -
+ * 1.er Donde me filtren por age los mentores
+ * 2.do Donde obtengamos un mentor en especifico con su nombre
+ */
+///ejercicio I
+app.get("/mentor", async (req, res) => {
+    const { age } = req.query;
+    const db = await fsPromises.readFile("./users.json", "utf8"); // leemos base de datos
+    const parsedDB = JSON.parse(db); // parseamos json
+    const filteredMentors = parsedDB.mentors.filter(mentor =>  mentor.age===age)
+    // Esta vacio, no filtro
+    if(age){
+    if(filteredMentors.length === 0) {
+        res.json({message:"usuario no encontrado"})
+    } else {
+        res.json(filteredMentors); // respondemos con header de Content-Type -> application/json
+    }
+    } else{
+        res.json(parsedDB.mentors)
+    }
+});
+
+app.get("/NameMentor", async (req, res) => {
+    const { name } = req.query;
+    const db = await fsPromises.readFile("./users.json", "utf8"); // leemos base de datos
+    const parsedDB = JSON.parse(db); // parseamos json
+    const filteredMentors = parsedDB.mentors.filter(mentor => name === mentor.name)
+    // Esta vacio, no filtro
+    if(filteredMentors.length === 0) {
+        res.json(parsedDB.mentors)
+    } else {
+        res.json(filteredMentors); // respondemos con header de Content-Type -> application/json
+    }
+});
+
 
 app.get("/koders/:name", async (req, res) => {
     // Path param
