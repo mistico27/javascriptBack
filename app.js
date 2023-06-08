0ec35d564cf512eb005afcfa7079cfb6552a9135
chris_beltran <cristianb72@hotmail.com>
2023-06-07 22:19:34 -0600

    mistico

--- app.js
+++ app.js
@@ -1,122 +1,60 @@
-const express = require('express')
-let app= express();
-const fsPromises = require("fs/promises");
-app.use(express.json()) // Parsear todo lo que el cliente me manda a JSON
+const express = require("express");
+const app = express();
 
-app.get("/",(req,res)=>{
-	res.send("mensaje desde el nuevo rest")
-})
+const mongoose = require('mongoose');
 
+const url='mongodb+srv://cristianb72:30vxY4BnKpy54mj7@cluster0.wxjolyf.mongodb.net/nuevoSchema?retryWrites=true&w=majority'
+mongoose.connect(url,{
+}).then(()=>{console.log("estoy conectado a la base  de datos de mongo carnalitos");})
+.catch((e)=>console.log("El error de conexion es " +e));
 
-  app.get("/getAllKoders", async (req, res) => {
-    const { module } = req.query;
-    const db = await fsPromises.readFile("./users.json", "utf8"); // leemos base de datos
-    const parsedDB = JSON.parse(db); // parseamos json
-    const filteredKoders = parsedDB.koders.filter(koder => module === koder.module)
-    // Esta vacio, no filtro
-    if(filteredKoders.length === 0) {
-        res.json(parsedDB.koders)
-    } else {
-        res.json(filteredKoders); // respondemos con header de Content-Type -> application/json
+app.use(express.json());
+const koderSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minlength: 3,
+      maxlength: 10,
+      required: true
+    },
+    module: {
+      type: String
+    },
+    generation: {
+      type: String
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+      max: 100
+    },
+    sex: {
+      type: String,
+      enum: ["f", "m", "o"]
     }
-});
-
-/**
- * Ejercicio
- * 2 endpoints -
- * 1.er Donde me filtren por age los mentores
- * 2.do Donde obtengamos un mentor en especifico con su nombre
- */
-///ejercicio I
-app.get("/mentor", async (req, res) => {
-    const { age } = req.query;
-    const db = await fsPromises.readFile("./users.json", "utf8"); // leemos base de datos
-    const parsedDB = JSON.parse(db); // parseamos json
-    const filteredMentors = parsedDB.mentors.filter(mentor =>  mentor.age===age)
-    // Esta vacio, no filtro
-    if(age){
-    if(filteredMentors.length === 0) {
-        res.json({message:"usuario no encontrado"})
-    } else {
-        res.json(filteredMentors); // respondemos con header de Content-Type -> application/json
-    }
-    } else{
-        res.json(parsedDB.mentors)
-    }
-});
-
-app.get("/NameMentor", async (req, res) => {
-    const { name } = req.query;
-    const db = await fsPromises.readFile("./users.json", "utf8"); // leemos base de datos
-    const parsedDB = JSON.parse(db); // parseamos json
-    const filteredMentors = parsedDB.mentors.filter(mentor => name === mentor.name)
-    // Esta vacio, no filtro
-    if(filteredMentors.length === 0) {
-        res.json(parsedDB.mentors)
-    } else {
-        res.json(filteredMentors); // respondemos con header de Content-Type -> application/json
-    }
-});
-
-
-///Register
-app.post("/koders", async (req, res) => {
-    const db = await fsPromises.readFile("./users.json", "utf8");
-    const parsedDb = JSON.parse(db);
-    const koder = {
-        id: parsedDb.koders.length + 1,
-        ...req.body
-    }
-
-    parsedDb.koders.push(koder);
-    await fsPromises.writeFile("./users.json", JSON.stringify(parsedDb, "\n", 4));
-    
-    res.json(koder)
-})
-
-////editar usuario
-app.patch("/koders/:id", async (req, res) => {
-    // Parametros
-    const { id } = req.params;
-    // Acceder a nuestra base de datos
-    const db = await fsPromises.readFile("./users.json", "utf8");
-    const parsedDb = JSON.parse(db);
-    // Encontrar indice de koder a actualizar
-    const index = parsedDb.koders.findIndex(koder=>koder.id===parseInt(id))
-
-  
-    // Crear nuestro objeto nuevo
-    const updatedKoder = {
-      ...parsedDb.koders[index], // ponme todo lo que ya tengas
-      ...req.body // agregame lo nuevo
-    }
-    // Actualizamos con el indice en la base de datos
-    parsedDb.koders[index] = updatedKoder
-  
-    // @ts-ignore
-    // Escribimos ya actualiza en nuestra base de datos
-    await fsPromises.writeFile("./users.json", JSON.stringify(parsedDb, "\n", 4));
-  
-    // Response con el koder actualizado
-    res.json(updatedKoder);
   })
+
+////modelo mayuscula y singular
+const Koders = mongoose.model("koders", koderSchema)
     
+app.get("/",(req,res)=>{
+    res.json("hey como abndas");
+})
+    
+app.get("/allUsers/:id",async(req,res)=>{
+    try{
+        const  koder = await Koders.findById(req.params.id);
+        const{...others}=koder._doc
+        console.log(koder);
+
+        res.status(200).json(others)
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 
 
-
-
-app.get("/koders/:name", async (req, res) => {
-    // Path param
-    const { name } = req.params;
-    const db = await fsPromises.readFile("./users.json", "utf8");
-    const parsedDb = JSON.parse(db);
-    const filteredKoder = parsedDb.koders.filter(
-        (koder) => koder.name.toLowerCase() === name.toLowerCase()
-    )[0];
-    res.json(filteredKoder);
-});
-
-app.listen(8000,()=>{
-    console.log("Estoy conectado")
+app.listen(8800,()=>{
+    console.log("BackEnd server is about to FIRE   !!!!");
 })
